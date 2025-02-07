b8f3130e3a3238d604937aa89a55ffc36465a1e2
herzen-alex <herzen.alex1@web.de>
2025-02-07 11:22:10 +0100

    added log_in

--- js/log_in.js
+++ js/log_in.js
@@ -1,7 +1,14 @@
 function init () {
-
+    logoMuve();
 }
 
 function toSignUp() {
     window.location.href = "./sign_up.html"
+}
+
+function logoMuve() {
+    setTimeout(() => {
+        document.getElementById('animation').style.display = "none";
+        document.getElementById('logo').style.display = "block";
+    }, 1500);
 }

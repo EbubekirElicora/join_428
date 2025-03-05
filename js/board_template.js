8a9862e436daab0950cdd85f3ba3477efd162e30
herzen-alex <herzen.alex1@web.de>
2025-03-05 20:59:48 +0100

    added board

--- js/board_template.js
+++ js/board_template.js
@@ -2,12 +2,30 @@
 
 
 function generateTodoHTML(task) {
-    return `<div draggable="true" ondragstart="startDragging('${task.id}')" class="task">
-                <h3>${task.category}</h3>
-                <p>${task.title}</p>
-                <p>${task.description || ''}</p>
-                <p>Due: ${task.dueDate}</p>
-                <p>priority: ${task.priority}</p>
+    const priorityIcons = {
+        urgent: '<img src="../assets/icons/prio_urgent_icon.png" alt="">',
+        medium: '<img src="../assets/icons/prio_medium_icon.png" alt="">',
+        low: '<img src="../assets/icons/prio_low_icon.png" alt="">'
+    };
+    const contactsHTML = task.assignedContacts?.map(contact => `
+        <div class="contact-badge" style="background-color: ${contact.color}">
+            ${contact.initials}
+        </div>
+    `).join('') || '';
 
-            </div>`;
+    return `
+        <div draggable="true" ondragstart="startDragging('${task.id}')" class="task">
+            <div class="category bg_${task.category}">${task.category}</div>
+            <h2>${task.title}</h2>
+            <p>${task.description || ''}</p>
+            <div class="priority_contact">
+                ${task.assignedContacts?.length > 0 ? `
+                    <div class="assigned-contacts">
+                        ${contactsHTML}
+                    </div>
+                ` : ''}
+                <span class="priority">${priorityIcons[task.priority]}</span>   
+            </div>
+        </div>
+    `;
 }

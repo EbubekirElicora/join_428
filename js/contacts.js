1acb0902fe7392e3f051c225b84d964e535e94f6
EbubekirElicora <ebubekir.elicora@gmx.de>
2025-03-16 01:00:44 +0100

    css contact

--- js/contacts.js
+++ js/contacts.js
@@ -12,9 +12,6 @@ if (closeButton) {
     });
 }
 
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const BASE_URL = "https://join-428-default-rtdb.europe-west1.firebasedatabase.app/";
 
